6305569f34b4e5dc4e923b6c7a85ffbbe0e9348c
Anas Ribki <anasribki147@gmail.com>
2024-03-14 20:22:04 +0000

    Edit task: 0. Prime Game

--- 0x0A-primegame/0-prime_game.py
+++ 0x0A-primegame/0-prime_game.py
@@ -9,19 +9,25 @@ def isWinner(x, nums):
     if not nums or x < 1:
         return None
 
-    prime = [False, False] + [True for _ in range(x - 1)]
-    for i in range(2, int(x ** 0.5) + 1):
-        if prime[i]:
-            for j in range(i * i, x + 1, i):
-                prime[j] = False
+    max_num = max(nums)
+    sieve = [0, 0] + [1 for _ in range(max_num - 1)]
+    for i in range(2, int(max_num ** 0.5) + 1):
+        if sieve[i]:
+            for j in range(i * i, max_num + 1, i):
+                sieve[j] = 0
 
-    prime = [i for i, j in enumerate(prime) if j]
+    sieve = [i for i in range(max_num + 1) if sieve[i]]
+    wins = {"Maria": 0, "Ben": 0}
+    for num in nums:
+        primes = sum(i <= num for i in sieve)
+        if primes % 2 == 0:
+            wins["Ben"] += 1
+        else:
+            wins["Maria"] += 1
 
-    n = 0
-    for i in nums:
-        if i in prime:
-            n += 1
-
-    if n % 2 == 0:
+    if wins["Maria"] > wins["Ben"]:
         return "Maria"
-    return "Ben"
+    elif wins["Ben"] > wins["Maria"]:
+        return "Ben"
+    else:
+        return None
